20656811861ff821c6adad6d9bead3d1feff8a34
eyalkapah <32191482+eyalkapah@users.noreply.github.com>
2018-10-29 08:46:20 +0200

    add login

---
+++ src/components/common/form.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import Input from "./input";
+import Joi from "joi-browser";
+
+class Form extends Component {
+  state = { data: {}, errors: {} };
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const result = Joi.validate(this.state.data, this.schema, options);
+
+    if (!result.error) return null;
+
+    const errors = {};
+
+    for (const item of result.error.details) {
+      errors[item.path[0]] = item.message;
+    }
+
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value }; // computed property in ES6
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+
+    return error ? error.details[0].message : null;
+  };
+
+  handleSubmit = e => {
+    e.preventDefault(); // prevent the submission of the form in order to prevent full reload
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+
+    if (errors) return;
+
+    this.doSubmit();
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const { data } = { ...this.state };
+    data[input.name] = input.value;
+    this.setState({ data, errors });
+  };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderInput(name, label, type = "text") {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        type={type}
+        name={name}
+        label={label}
+        value={data[name]}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+}
+
+export default Form;
